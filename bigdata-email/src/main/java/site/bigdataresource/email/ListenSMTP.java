4fce432d034dc1e5a7c21f31fb83031759466bee
qindeqiang <qindeqiang1@163.com>
2017-09-08 11:00:50 -0400

    Add News Module

--- bigdata-email/src/main/java/site/bigdataresource/email/ListenSMTP.java
+++ bigdata-email/src/main/java/site/bigdataresource/email/ListenSMTP.java
@@ -4,4 +4,9 @@ package site.bigdataresource.email;
  * Created by bigdata on 9/8/17.
  */
 public class ListenSMTP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,Bigdata Resource Email");
+    }
+
 }
