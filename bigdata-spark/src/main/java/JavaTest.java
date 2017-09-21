98a2b62a0ddb7a54fdcb1e3de8765d0c73764ce7
qindeqiang <qindeqiang1@163.com>
2017-09-20 23:28:40 -0400

    9.21 Modify Spark Module

---
+++ bigdata-spark/src/main/java/JavaTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by deqiangqin@gmail.com on 9/20/17.
+ */
+public class JavaTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
+
---
+++ bigdata-sqoop/src/main/java/site/bigdataresource/sqoop/JavaHelloWorld.java
@@ -0,0 +1,10 @@
+package site.bigdataresource.sqoop;
+
+/**
+ * Created by deqiangqin@gmail.com on 9/20/17.
+ */
+public class JavaHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloJava");
+    }
+}
