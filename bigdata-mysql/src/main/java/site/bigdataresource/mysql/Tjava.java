fc73ad39815434128d8df6f7d8e94c43ad9c67e1
qindeqiang <qindeqiang1@163.com>
2017-09-20 23:44:23 -0400

    9.21 Modify Spark Module to Spark2

--- bigdata-mysql/src/main/java/site/bigdataresource/mysql/Tjava.java
+++ bigdata-mysql/src/main/java/site/bigdataresource/mysql/Tjava.java
@@ -1,10 +1,10 @@
+package site.bigdataresource.mysql;
+
 /**
  * Created by deqiangqin@gmail.com on 9/20/17.
  */
-public class JavaTest {
-
+public class Tjava {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello TJAVA");
     }
 }
-
---
+++ bigdata-spark2/src/main/java/site/bigdataresource/spark/TJava.java
@@ -0,0 +1,10 @@
+package site.bigdataresource.spark;
+
+/**
+ * Created by deqiangqin@gmail.com on 9/20/17.
+ */
+public class TJava {
+    public static void main(String[] args) {
+        System.out.println("HelloJava");
+    }
+}
