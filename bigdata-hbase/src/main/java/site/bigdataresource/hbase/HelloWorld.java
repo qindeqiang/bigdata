be9aff67e335844c2a3c6bf64003167c09102f04
qindeqiang <qindeqiang1@163.com>
2017-10-10 03:54:22 -0400

    10.10 Add Test File

---
+++ bigdata-hbase/src/main/java/site/bigdataresource/hbase/HelloWorld.java
@@ -0,0 +1,10 @@
+package site.bigdataresource.hbase;
+
+/**
+ * Created by deqiangqin@gmail.com on 10/10/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello deqiang qin");
+    }
+}
